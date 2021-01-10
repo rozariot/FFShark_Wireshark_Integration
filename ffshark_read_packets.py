764cdc755740a07d59d449683a33da9d6bdead7c
Tobias <tobi.rozario@gmail.com>
2021-01-10 01:39:43 +0000

    added locking mechanism to the files. Now need to do end to end testing with multiple packets

--- ffshark_read_packets.py
+++ ffshark_read_packets.py
@@ -9,6 +9,8 @@ import io
 import math
 from scapy.all import *
 from scapy.layers.http import *
+import fcntl
+
 # import libmpsoc provided by Clark
 # make sure it's installed as python module by running sudo pip3 install libmpsoc
 # should already be installed but incase it isn't refer to above comment
@@ -23,30 +25,46 @@ FIFO_RDFD_OFFSET = 0x20
 FIFO_RLR_OFFSET = 0x24
 FIFO_DATA_WIDTH = 4 #in bytes
 
+# Lock file used to maintain thread safety for multiple 
+# processes running on the FPGA
+LOCK_FILE = "/var/lock/pokelockfile"
+
+def init_lock_file():
+    if not os.path.exists(LOCK_FILE):
+        os.system("touch " + LOCK_FILE)
+
 def main():
+    # initialize the lock
+    init_lock_file()
+    lock = open(LOCK_FILE, "r")
 
     # init the axi lite mem map for FIFO
+    fcntl.flock(lock.fileno(), fcntl.LOCK_EX)
     axil_FIFO = mp.axilite(addr=FIFO_BASE,size=FIFO_SIZE)
+    fcntl.flock(lock.fileno(), fcntl.LOCK_UN)
     
     file_str = ""
     
     # check if Receive FIFO has packets to read, if not don't read
+    fcntl.flock(lock.fileno(), fcntl.LOCK_EX)
     num_words_in_FIFO = axil_FIFO.read32(offset=FIFO_RDFO_OFFSET)
-    print("num words in fifo " + str(num_words_in_FIFO))
+    fcntl.flock(lock.fileno(), fcntl.LOCK_UN)
+    #print("num words in fifo " + str(num_words_in_FIFO))
     if (num_words_in_FIFO != 0):
         # read the number of bytes in a packet, this can be less than the num_words_in_FIFO
+        fcntl.flock(lock.fileno(), fcntl.LOCK_EX)
         num_bytes_in_packet = axil_FIFO.read32(offset=FIFO_RLR_OFFSET)
-        print(num_bytes_in_packet)
+        #print(num_bytes_in_packet)
         # this rounds up so we read the next partial words
         # currently this doesn't work as FFShark sends a copy of the 2nd last word
         num_words_in_packet = int(math.ceil(num_bytes_in_packet/4))
         # read the data and convert to hex string
         for i in range(num_words_in_packet):
             data_word = axil_FIFO.read32(offset=FIFO_RDFD_OFFSET)
-            print(hex(data_word))
+            #print(hex(data_word))
             file_str = file_str + "{:08x}".format(data_word)
-            
-        print(file_str)
+        fcntl.flock(lock.fileno(), fcntl.LOCK_UN)    
+        #print(file_str)
         packet = Ether(binascii.a2b_hex(file_str))
         wrpcap('test.pcap', packet)
         with open('test.pcap', 'rb') as file:
--- ffshark_send_packets.py
+++ ffshark_send_packets.py
@@ -7,6 +7,8 @@ import sys
 import glob
 import time
 import math
+import fcntl
+
 # import libmpsoc provided by Clark
 # make sure it's installed as python module by running sudo pip3 install libmpsoc
 # should already be installed but incase it isn't refer to above comment
@@ -29,6 +31,14 @@ FIFO_SRR_OFFSET = 0x28
 FIFO_SRR_RST_VAL = 0xA5
 FIFO_DATA_WIDTH = 4 #in bytes
 
+# Lock file used to maintain thread safety for multiple 
+# processes running on the FPGA
+LOCK_FILE = "/var/lock/pokelockfile"
+
+def init_lock_file():
+	if not os.path.exists(LOCK_FILE):
+		os.system("touch " + LOCK_FILE)
+
 def main():
     parser = argparse.ArgumentParser(description="Sends packets randomly one by one to ffshark when given packet text files directory")
     parser.add_argument("--packets-directory", action="store", help="Provide directory of packet text files", required="true")
@@ -45,6 +55,12 @@ def main():
     directory = os.path.join(directory, '')
     packet_files_list = glob.glob(directory + "*.txt")
 
+    # initialize the lock
+    init_lock_file()
+    lock = open(LOCK_FILE, "r")
+
+    fcntl.flock(lock.fileno(), fcntl.LOCK_EX)
+
     # init the axi lite mem map for FFShark and FIFO
     axil_FFShark = mp.axilite(addr=FFSHARK_BASE,size=FFSHARK_SIZE)
     axil_FIFO = mp.axilite(addr=FIFO_BASE,size=FIFO_SIZE)
@@ -52,6 +68,8 @@ def main():
     # initialize FFShark and FIFO
     print(axil_FFShark.write32(value=0x1,offset=FFSHARK_ENABLE_OFFSET))
     print(axil_FIFO.write32(value=FIFO_SRR_RST_VAL,offset=FIFO_SRR_OFFSET))
+
+    fcntl.flock(lock.fileno(), fcntl.LOCK_UN)	
 
     # sending packets
     iteration_count = 0
@@ -64,13 +82,18 @@ def main():
         # divide by 8 because each hex char is UTF-8 so 1 byte but it represents only 4 bits so it's 2x the data
         num_words = int(math.ceil(size_bytes/8))
         print(num_words)
+        fcntl.flock(lock.fileno(), fcntl.LOCK_EX)
         num_vacant_words = axil_FIFO.read32(offset=FIFO_TDFV_OFFSET)
+        fcntl.flock(lock.fileno(), fcntl.LOCK_UN)
         print("num vacant words ::: " + str(num_vacant_words))
         # check if the FIFO has enough space for the packet and if not wait
         while (num_words > num_vacant_words):
+            fcntl.flock(lock.fileno(), fcntl.LOCK_EX)
             num_vacant_words = axil_FIFO.read32(offset=FIFO_TDFV_OFFSET)
+            fcntl.flock(lock.fileno(), fcntl.LOCK_UN)
             print("num vacant words ::: " + str(num_vacant_words)) 
         # write the packet a word at a time
+        fcntl.flock(lock.fileno(), fcntl.LOCK_EX)
         with open(file) as f:
             for i in range(num_words):
                 file_val = int(f.read(8),16)
@@ -80,6 +103,7 @@ def main():
         # divide by 2 because each hex char is UTF-8 so 1 byte but it represents only 4 bits so it's 2x the data
         print(int(math.ceil(size_bytes/2)))
         print(axil_FIFO.write32(value=int(math.ceil(size_bytes/2)),offset=FIFO_TLR_OFFSET))
+        fcntl.flock(lock.fileno(), fcntl.LOCK_UN)        
         if (wait_time > 0):
             time.sleep(wait_time)
         iteration_count += 1
