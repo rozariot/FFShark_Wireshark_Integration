7ffdc186b312c6c319d331065edeb7b5ec7fd3a8
Tobias <tobi.rozario@gmail.com>
2021-03-15 19:36:38 +0000

    added locking mechanism library code. need to test this with main code

--- ffshark_c_lang_drivers/libmpsoc.c
+++ ffshark_c_lang_drivers/libmpsoc.c
@@ -1,6 +1,48 @@
 #include "libmpsoc.h"
 
+//Locking Mechanism APIs
+//references: 
+//https://man7.org/tlpi/code/online/dist/filelock/t_flock.c.html
+//https://manpages.debian.org/buster/manpages-dev/flock.2.en.html
+void lock_init(){
+    /*
+    Locking file function. Returns file descriptor.
+        
+    example:
+    lock_init()
+    int fd = lock() //locks
+        
+    ... some code operations ...
+        
+    unlock(fd); //unlock with the unlock function
+    
+    */    
+    FILE *file = fopen((char *)LOCK_FILE, "r");
+    
+    if (!file){
+        system(MAKE_LOCK_FILE_CMD);        
+    }
+    else {
+        fclose(file);
+    }
+}
 
+int lock(){
+    //example usage in description under lock_init()
+    int fd;  
+    fd = open (LOCK_FILE, O_RDONLY);
+    if (fd == -1) perror("Lock file not found"); 
+    flock(fd, LOCK_EX);
+    return fd; 
+}
+
+void unlock(int fd){
+    //example usage in description under lock_init()    
+    flock(fd, LOCK_UN);
+    close(fd);
+}
+
+// Axilite APIs
 void init_axilite(AXILITE *axilite_handler, unsigned addr, unsigned size){
     axilite_handler->page_size = sysconf(_SC_PAGE_SIZE);
     
--- ffshark_c_lang_drivers/libmpsoc.h
+++ ffshark_c_lang_drivers/libmpsoc.h
@@ -9,8 +9,19 @@
 #include <string.h> //perror, sprintf
 #include <errno.h> 
 #include <sys/mman.h> //mmap
+#include <sys/file.h> //flock
 
+//Lock file and commond to create lock file
+#define LOCK_FILE "/var/lock/pokelockfile"
+#define MAKE_LOCK_FILE_CMD "touch /var/lock/pokelockfile"
 
+//APIs that use lock file to maintain thread safety 
+//for multiple processes running on FPGA.
+void lock_init();
+int lock();
+void unlock(int fd);
+
+// Axilite APIs. Works specifically with 32 bit datawidth
 typedef struct {
     unsigned size;
     int fd;
@@ -23,5 +34,8 @@ void init_axilite(AXILITE *axilite_handler, unsigned addr, unsigned size);
 unsigned read_axilite(AXILITE *axilite_handler, unsigned offset);
 
 void write_axilite(AXILITE *axilite_handler, unsigned offset, unsigned data);
+
+ 
+
 
 #endif
